65deb6364876e781044ee337bd4fc0f1de806a55
Merhdev <office.merh@gmail.com>
2022-09-01 14:21:03 +0300

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Button } from "react-bootstrap";
-import Header from "./components/Header"
-import Form from "./pages/Form"
+import { Button } from 'react-bootstrap';
+import Header from './components/Header';
+import Form from './pages/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
 export default function App() {
   return (
-    <div className='app-wrapper'>
-   {/*<h1> Helo World </h1> */}
+    <div className="app-wrapper">
+      <h1> Work, work </h1>
       <Button variant="primary">Button</Button>
       <Header />
       <Form />
